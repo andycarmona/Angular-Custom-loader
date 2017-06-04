d8c0bed224a802b30948aee372a86fe49b113663
andycarmona <andres.carmona@uni-app.se>
2017-06-04 19:24:43 +0200

    [] Custom loader to use property files wit ngx-translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule,Http } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import {TranslateModule, TranslateLoader} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+export function createTranslateLoader(http: Http) {
+    return new TranslateHttpLoader(http, './assets/i18n/', '.properties');
+}
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +16,14 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    TranslateModule.forRoot({
+      loader:{
+        provide:TranslateLoader,
+        useFactory:(createTranslateLoader),
+        deps:[Http]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
